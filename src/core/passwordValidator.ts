4c3345ae6c7c4bef2b0c54b1210b8a6f68886be3
Luis <luis.amez.vitoria@gmail.com>
2026-01-13 17:35:32 +0100

    Implement isPasswordSafe functionality

--- src/core/passwordValidator.ts
+++ src/core/passwordValidator.ts
@@ -1,3 +1,30 @@
-export function passwordValidator() {
-  return true;
+export function isPasswordSafe(password: string) {
+  const MINIMUM_PASSWORD_LENGTH = 6;
+  return (
+    lengthEqualOrGreaterThan(password, MINIMUM_PASSWORD_LENGTH) &&
+    containsDigit(password) &&
+    containsUpperCase(password) &&
+    containsLowerCase(password) &&
+    containsUnderscore(password)
+  );
+}
+
+function lengthEqualOrGreaterThan(password: string, length: number) {
+  return password.length >= length;
+}
+
+function containsDigit(password: string) {
+  return /\d/.test(password);
+}
+
+function containsUpperCase(password: string) {
+  return /[A-Z]/.test(password);
+}
+
+function containsLowerCase(password: string) {
+  return /[a-z]/.test(password);
+}
+
+function containsUnderscore(password: string) {
+  return /_/.test(password);
 }
--- src/tests/passwordValidator.test.ts
+++ src/tests/passwordValidator.test.ts
@@ -1,7 +1,51 @@
-import { passwordValidator } from '../core/passwordValidator';
+import { isPasswordSafe } from '../core/passwordValidator';
 
-describe('passwordValidator', () => {
-  it('should return true', () => {
-    expect(passwordValidator()).toBe(true);
+describe('isPasswordSafe', () => {
+  it('fails if the password is not long enough', () => {
+    const password = 'a1C_e';
+
+    const result = isPasswordSafe(password);
+
+    expect(result).toBe(false);
+  });
+
+  it('fails if the password does not contain at least one number', () => {
+    const password = 'abC_ef';
+
+    const result = isPasswordSafe(password);
+
+    expect(result).toBe(false);
+  });
+
+  it('fails if the password does not contain at least one uppercase letter', () => {
+    const password = 'a1c_ef';
+
+    const result = isPasswordSafe(password);
+
+    expect(result).toBe(false);
+  });
+
+  it('fails if the password does not contain at least one lowercase letter', () => {
+    const password = 'A1C_EF';
+
+    const result = isPasswordSafe(password);
+
+    expect(result).toBe(false);
+  });
+
+  it('fails if the password does not contain an underscore', () => {
+    const password = 'a1Cdef';
+
+    const result = isPasswordSafe(password);
+
+    expect(result).toBe(false);
+  });
+
+  it('succeeds if the password fulfills all the security criteria', () => {
+    const password = 'a1C_ef';
+
+    const result = isPasswordSafe(password);
+
+    expect(result).toBe(true);
   });
 });
